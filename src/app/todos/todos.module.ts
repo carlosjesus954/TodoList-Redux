49f6f5943dae6935229c515f7f55f097a3eb530e
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-07-29 23:03:38 +0200

    Todo App primer comit

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodosModule { }
