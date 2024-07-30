322ce5e9a62d6b5ad1954d4c96654119e4f39d0d
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-07-30 22:22:44 +0200

    Maquetacion lista todoList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { TodoPageComponent } from './todos/todo-page/todo-page.component';
+import { TodosModule } from './todos/todos.module';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, TodosModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
